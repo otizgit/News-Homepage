3e67c50428b2b9abca2acab3988a0e316a2553be
Emmanuel <emmanueljoe777@gmail.com>
2023-03-11 11:47:28 +0100

    nav changes

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
 const list = document.querySelector(".list");
-const listItems = document.querySelectorAll("nav ul li");
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
 const overlay = document.querySelector(".overlay");
 
-hamburger.addEventListener("click", function () {
+hamburger.addEventListener("click", showSideBar);
+
+close.addEventListener("click", removeSideBar);
+
+overlay.addEventListener("click", removeSideBar);
+
+function showSideBar() {
   list.classList.toggle("open");
-  listItems.forEach(function (listItem) {
-    listItem.classList.add("animate");
-  });
-  close.classList.toggle("animate");
-  overlay.classList.add('show-overlay')
-});
+  overlay.classList.add("show-overlay");
+}
 
-close.addEventListener("click", function () {
+function removeSideBar() {
   list.classList.remove("open");
-  close.classList.remove("animate");
-  listItems.forEach(function (listItem) {
-    listItem.classList.remove("animate");
-  });
-  overlay.classList.remove('show-overlay')
-});
-
-overlay.addEventListener('click', function(){
-    list.classList.remove("open");
-    close.classList.remove("animate");
-    listItems.forEach(function (listItem) {
-      listItem.classList.remove("animate");
-    });
-    overlay.classList.remove('show-overlay');
-})
+  overlay.classList.remove("show-overlay");
+}
